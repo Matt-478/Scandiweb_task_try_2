2b5326369870e5cace974bb26d78fc5bbfd0830c
Matīss Komļevs <matisskomlevs@gmail.com>
2022-02-20 15:51:38 +0200

    React connected with Apollo client and GraphQL

--- try-2/src/App.js
+++ try-2/src/App.js
@@ -1,9 +1,30 @@
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from } from '@apollo/client'
+import { ErrorLink, onError } from '@apollo/client/link/error'
+
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({message, location, path}) => {
+      alert(`GraphQL error ${message}`)
+    })
+  }
+})
+
+const link = from([
+  errorLink,
+  new HttpLink({uri: 'http//localhost:4000'}),
+])
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link
+})
+
 
 function App() {
   return (
-    <div className="App">
-      React app
-    </div>
+    <ApolloProvider client={client}>
+      <div>React app</div>
+    </ApolloProvider>
   );
 }
 
